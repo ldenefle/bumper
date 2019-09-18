65c5b0bfd3862d1adbda91572dbae4bb01e91ae2
Lucas Denefle <lucas.denefle@converge.io>
2019-09-18 11:18:15 +0100

    feat: first commit, seems functional

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use git2::{Repository, Error};
+use semver::{Version, SemVerError};
+use std::cmp::Ordering;
+use regex::Regex;
+
+#[derive(PartialEq)]
+enum BumpType {
+    Major,
+    Minor,
+    Patch
+}
+
+fn main() {
+    let repo = Repository::open(".").unwrap();
+    let last_tag = match get_version_from_tag(&repo) {
+        Ok(version) => version,
+        Err(e) => {
+            panic!("error : {}", e);
+        }
+    };
+
+    let mut current_version = validate_tag(&last_tag).unwrap();
+    println!("Current version is {}", current_version);
+
+    match get_bump_type(&repo, &last_tag) {
+        Ok(BumpType::Major) => current_version.increment_major(),
+        Ok(BumpType::Minor) => current_version.increment_minor(),
+        Ok(BumpType::Patch) => current_version.increment_patch(),
+        Err(e) => panic!("error: {}", e),
+    };
+
+    println!("Next version is {}", current_version);
+}
+
+fn get_bump_type(repo: &Repository, tag: &str) -> Result<BumpType, Error> {
+    let mut bump = BumpType::Patch;
+    let mut revwalk = repo.revwalk()?;
+    revwalk.push_range(format!("{}..HEAD", &tag).as_str())?;
+    revwalk.set_sorting(git2::Sort::TOPOLOGICAL);
+    for rev in revwalk {
+        let commit = repo.find_commit(rev?)?;
+        let message = commit.summary_bytes().unwrap_or_else(|| commit.message_bytes());
+        let message = String::from_utf8_lossy(message);
+        if message.contains("BREAKING CHANGE") {
+            return Ok(BumpType::Major)
+        }
+        let re = Regex::new(r"^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\([a-z ]+\))?: [\w ]+$").unwrap();
+        let commit_type = match re.captures(&message) {
+            Some(caps) => caps.get(1).map_or(tag, |m| m.as_str()),
+            None => return Err(Error::from_str(format!("This commit is not conventional: {}", message).as_str())),
+        };
+
+        let commit_bump = match commit_type {
+            "feat" => BumpType::Minor,
+            "fix" => BumpType::Patch,
+            _ => BumpType::Patch,
+        };
+
+        if bump == BumpType::Patch && commit_bump == BumpType::Minor {
+            bump = BumpType::Minor
+        }
+
+    }
+    Ok(bump)
+}
+
+fn validate_tag(tag: &str) -> Result<semver::Version, SemVerError> {
+    let re = Regex::new(r"v(.*)").unwrap();
+    let tag = match re.captures(tag) {
+        Some(caps) => caps.get(1).map_or(tag, |m| m.as_str()),
+        None => tag,
+    };
+    println!("Validating tag {}", tag);
+    Version::parse(tag)
+}
+
+fn get_version_from_tag(repo: &Repository) -> Result<String, Error> {
+    let tags = repo.tag_names(Some("*"))?;
+    // If no tags detected, return first tag
+    if tags.is_empty() {
+        Err(Error::from_str("No tags detected in repo"))
+    }
+    // If only one tag detected check if valid and returns
+    else if tags.len() == 1 {
+        Ok(String::from(tags.get(0).unwrap()))
+    }
+    // If multiple tags detected gets the most recent
+    else {
+        let max_tag = tags.iter().max_by(|x, y| {
+            if let (Some(x), Some(y)) = (x, y) {
+                println!("{} {}", x, y);
+                let v1 = validate_tag(x).unwrap();
+                let v2 = validate_tag(y).unwrap();
+                return v1.cmp(&v2);
+            }
+            Ordering::Greater
+        }).unwrap();
+
+        println!("Older tag is {}", max_tag.unwrap());
+        Ok(String::from(max_tag.unwrap()))
+    }
+}
+
